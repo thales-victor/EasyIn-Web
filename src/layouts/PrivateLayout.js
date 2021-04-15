102cac6445c694b69c9f4f418a2d57d0d7be3001
Thales Victor <thalesv.victor@gmail.com>
2021-04-15 20:00:25 -0300

    Redirecionamento automático para tela de login caso não logado

--- src/layouts/PrivateLayout.js
+++ src/layouts/PrivateLayout.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Toolbar from '../components/layout/toolbar';
+import { GetAuthentication } from '../services/localStorage/LocalStorageService';
+import { useHistory } from "react-router-dom";
 
-function PrivateLayout({children}) {
+function PrivateLayout({ children }) {
+  
+  const history = useHistory();
+
+  useEffect(() => {
+    const token = GetAuthentication();
+    if (!token) {
+      history.push("/login");
+    }
+  })
+
   return (
     <>
       <Toolbar />
